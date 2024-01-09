6ad3378b5801fd9d87110646bb28c9a64340df34
efm18 <efm18@LAPTOP-PGGMSI29>
2024-01-09 11:16:50 +0100

    capitan and magnificat splitted when space and colon

--- data/config.py
+++ data/config.py
@@ -7,7 +7,21 @@ DS_CONFIG = {
         "transcripts": "data/b-59-850/GT",
         "images": "data/b-59-850/Images",
     },
+    "b-59-850_colon": {
+        "train": "data/b-59-850/Folds/train_gt_fold0.dat",
+        "val": "data/b-59-850/Folds/val_gt_fold0.dat",
+        "test": "data/b-59-850/Folds/test_gt_fold0.dat",
+        "transcripts": "data/b-59-850/GT",
+        "images": "data/b-59-850/Images",
+    },
     "Magnificat": {
+        "train": "data/Magnificat/Folds/train_gt_fold0.dat",
+        "val": "data/Magnificat/Folds/val_gt_fold0.dat",
+        "test": "data/Magnificat/Folds/test_gt_fold0.dat",
+        "transcripts": "data/Magnificat/GT",
+        "images": "data/Magnificat/Images",
+    },
+    "Magnificat_colon": {
         "train": "data/Magnificat/Folds/train_gt_fold0.dat",
         "val": "data/Magnificat/Folds/val_gt_fold0.dat",
         "test": "data/Magnificat/Folds/test_gt_fold0.dat",
