841431e253a3dbc6fceec70b0f8824b9bebe2f3a
María <56754030+mariaalfaroc@users.noreply.github.com>
2024-01-10 19:55:48 +0100

    added Malaga, Mottecta and Guatemala datasets

--- data/config.py
+++ data/config.py
@@ -7,21 +7,7 @@ DS_CONFIG = {
         "transcripts": "data/b-59-850/GT",
         "images": "data/b-59-850/Images",
     },
-    "b-59-850_colon": {
-        "train": "data/b-59-850/Folds/train_gt_fold0.dat",
-        "val": "data/b-59-850/Folds/val_gt_fold0.dat",
-        "test": "data/b-59-850/Folds/test_gt_fold0.dat",
-        "transcripts": "data/b-59-850/GT",
-        "images": "data/b-59-850/Images",
-    },
     "Magnificat": {
-        "train": "data/Magnificat/Folds/train_gt_fold0.dat",
-        "val": "data/Magnificat/Folds/val_gt_fold0.dat",
-        "test": "data/Magnificat/Folds/test_gt_fold0.dat",
-        "transcripts": "data/Magnificat/GT",
-        "images": "data/Magnificat/Images",
-    },
-    "Magnificat_colon": {
         "train": "data/Magnificat/Folds/train_gt_fold0.dat",
         "val": "data/Magnificat/Folds/val_gt_fold0.dat",
         "test": "data/Magnificat/Folds/test_gt_fold0.dat",
@@ -35,12 +21,19 @@ DS_CONFIG = {
         "transcripts": "data/ILS/GT",
         "images": "data/ILS/Images",
     },
-    "ILS_colon": {
-        "train": "data/ILS/Folds/train_gt_fold0.dat",
-        "val": "data/ILS/Folds/val_gt_fold0.dat",
-        "test": "data/ILS/Folds/test_gt_fold0.dat",
-        "transcripts": "data/ILS/GT",
-        "images": "data/ILS/Images",
+    "Guatemala": {
+        "train": "data/Guatemala/Folds/train_gt_fold0.dat",
+        "val": "data/Guatemala/Folds/val_gt_fold0.dat",
+        "test": "data/Guatemala/Folds/test_gt_fold0.dat",
+        "transcripts": "data/Guatemala/GT",
+        "images": "data/Guatemala/Images",
+    },
+    "Mottecta": {
+        "train": "data/Mottecta/Folds/train_gt_fold0.dat",
+        "val": "data/Mottecta/Folds/val_gt_fold0.dat",
+        "test": "data/Mottecta/Folds/test_gt_fold0.dat",
+        "transcripts": "data/Mottecta/GT",
+        "images": "data/Mottecta/Images",
     },
     # Modern
     "Primus": {
@@ -63,5 +56,12 @@ DS_CONFIG = {
         "test": "data/FMT/Folds/test_gt_fold0.dat",
         "transcripts": "data/FMT/GT",
         "images": "data/FMT/Images",
+    },
+    "Malaga": {
+        "train": "data/Malaga/Folds/train_gt_fold0.dat",
+        "val": "data/Malaga/Folds/val_gt_fold0.dat",
+        "test": "data/Malaga/Folds/test_gt_fold0.dat",
+        "transcripts": "data/Malaga/GT",
+        "images": "data/Malaga/Images",
     },
 }
