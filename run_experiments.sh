2a72e7dcbc3803dca2457643a37383ec11ba7a31
mariaalfaroc <mac77@alu.ua.es>
2024-01-19 16:06:17 +0000

    update run_experiments

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# BASELINE
 # - MENSURAL
 for encoding in standard split; do
     for train_ds in b-59-850 ILS Magnificat Guatemala Mottecta; do
@@ -8,6 +7,7 @@ for encoding in standard split; do
         for test_ds in b-59-850 ILS Magnificat Guatemala Mottecta; do
             if [ $train_ds != $test_ds ]; then
                 python -u test.py --train_ds_name $train_ds --test_ds_name $test_ds --checkpoint_path weights/Baseline-UpperBound/$train_ds\_$encoding.ckpt --encoding_type $encoding
+                python -u da_train_random_search.py --train_ds_name $train_ds --test_ds_name $test_ds --encoding_type $encoding --num_random_combinations 50
             fi
         done
     done
@@ -18,12 +18,15 @@ for encoding in standard split; do
         python -u train.py --ds_name $train_ds --encoding_type $encoding
         for test_ds in Primus CameraPrimus FMT Malaga; do
             if [ $train_ds != $test_ds ]; then
+                if [ $train_ds == Primus ] && [ $test_ds == CameraPrimus ]; then
+                    continue
+                fi
+                if [ $train_ds == CameraPrimus ] && [ $test_ds == Primus ]; then
+                    continue
+                fi
                 python -u test.py --train_ds_name $train_ds --test_ds_name $test_ds --checkpoint_path weights/Baseline-UpperBound/$train_ds\_$encoding.ckpt --encoding_type $encoding
+                python -u da_train_random_search.py --train_ds_name $train_ds --test_ds_name $test_ds --encoding_type $encoding --num_random_combinations 50
             fi
         done
     done
 done
-
-# AMD SOURCE-FREE DOMAIN ADAPTATION
-# DA example:
-python -u da_train.py --train_ds_name b-59-850 --test_ds_name ILS --checkpoint_path weights/Baseline-UpperBound/b-59-850_standard.ckpt --bn_ids 1,5,9,13 --align_loss_weight 10 --minimize_loss_weight 10 --diversify_loss_weight 10 --lr 0.0003 --encoding_type standard
