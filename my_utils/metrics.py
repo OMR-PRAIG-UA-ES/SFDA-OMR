8e3e7c27287dbf66690119102404283db16b1ef0
mariaalfaroc <mac77@alu.ua.es>
2024-04-28 13:06:05 +0200

    added type hints

--- my_utils/metrics.py
+++ my_utils/metrics.py
@@ -1,10 +1,11 @@
 import re
+
 import torch
 
 # -------------------------------------------- METRICS:
 
 
-def levenshtein(a, b):
+def levenshtein(a: list[str], b: list[str]) -> int:
     n, m = len(a), len(b)
 
     if n > m:
@@ -24,19 +25,18 @@ def levenshtein(a, b):
     return current[n]
 
 
-def compute_ser(y_true, y_pred):
+def compute_ser(y_true: list[list[str]], y_pred: list[list[str]]) -> float:
     ed_acc = 0
     length_acc = 0
     for t, h in zip(y_true, y_pred):
-        # Convert to split-sequence encoding to ensure a fair comparison between both encodings
-        t = re.split(r"\s+|:", " ".join(t))
-        h = re.split(r"\s+|:", " ".join(h))
         ed_acc += levenshtein(t, h)
         length_acc += len(t)
     return 100.0 * ed_acc / length_acc
 
 
-def compute_metrics(y_true, y_pred):
+def compute_metrics(
+    y_true: list[list[str]], y_pred: list[list[str]]
+) -> dict[str, float]:
     metrics = {"ser": compute_ser(y_true, y_pred)}
     return metrics
 
@@ -44,12 +44,18 @@ def compute_metrics(y_true, y_pred):
 # -------------------------------------------- CTC DECODERS:
 
 
-def ctc_greedy_decoder(y_pred, i2w):
+def ctc_greedy_decoder(
+    y_pred: torch.Tensor,
+    i2w: dict[int, str],
+    blank_padding_token: int,
+) -> list[str]:
     # y_pred = [seq_len, num_classes]
     # Best path
     y_pred_decoded = torch.argmax(y_pred, dim=1)
     # Merge repeated elements
     y_pred_decoded = torch.unique_consecutive(y_pred_decoded, dim=0).tolist()
-    # Convert to string; len(i2w) -> CTC-blank
-    y_pred_decoded = [i2w[i] for i in y_pred_decoded if i != len(i2w)]
+    # Convert to string (remove CTC blank token)
+    y_pred_decoded = [i2w[i] for i in y_pred_decoded if i != blank_padding_token]
+    # Convert to split-sequence encoding to ensure a fair comparison between both encodings
+    y_pred_decoded = re.split(r"\s+|:", " ".join(y_pred_decoded))
     return y_pred_decoded
